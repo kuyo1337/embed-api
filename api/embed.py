2ceb196816f04b119921a49f6807d0f702a23825
kuyo <prod.kuyo@proton.me>
2026-05-15 22:04:34 +0700

    Add files via upload

---
+++ api/embed.py
@@ -0,0 +1,54 @@
+from http.server import BaseHTTPRequestHandler
+from urllib.parse import urlparse, parse_qs
+
+
+class handler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        query = parse_qs(urlparse(self.path).query)
+
+        title = query.get("title", [""])[0]
+        description = query.get("description", [""])[0]
+        color = query.get("color", ["5865F2"])[0]
+        image = query.get("image", [""])[0]
+        url = query.get("url", [""])[0]
+        provider_name = query.get("provider_name", [""])[0]
+        provider_url = query.get("provider_url", ["https://google.com"])[0]
+
+        html = f"""
+<!DOCTYPE html>
+<html>
+<head>
+
+<meta charset="UTF-8">
+
+<meta property="og:type" content="website">
+<meta property="og:title" content="{title}">
+<meta property="og:description" content="{description}">
+<meta property="og:url" content="{url}">
+<meta property="og:image" content="{image}">
+<meta property="theme-color" content="#{color}">
+<meta property="og:site_name" content="{provider_name}">
+
+<meta name="twitter:card" content="summary_large_image">
+<meta name="twitter:title" content="{title}">
+<meta name="twitter:description" content="{description}">
+<meta name="twitter:image" content="{image}">
+
+<title>{title}</title>
+
+</head>
+<body>
+
+<script>
+window.location.href = "{provider_url}";
+</script>
+
+</body>
+</html>
+"""
+
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(html.encode())
